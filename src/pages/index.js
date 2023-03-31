4d376392c15df9e5d9c668f8656abcd88b83e62b
mfestrada <michaelxestrada@gmail.com>
2023-03-31 09:28:38 -0700

    adds google-analytics gtag.js call

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import TerminalWrapper from '../components/TerminalWrapper';
 import styles from '@/styles/Home.module.css';
 import 'xterm/css/xterm.css';
+import Script from 'next/script';
 
 export default function Home() {
   return (
@@ -14,6 +15,19 @@ export default function Home() {
       <main className={styles.main}>
         <TerminalWrapper />
       </main>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        src='https://www.googletagmanager.com/gtag/js?id=G-P3FDEX3XMJ'
+        strategy='afterInteractive'
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-P3FDEX3XMJ');
+        `}
+      </Script>
     </>
   )
 }
