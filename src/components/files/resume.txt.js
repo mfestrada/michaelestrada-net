283e4ab7a3d5101816e758e659e3ba512d291c50
mfestrada <michaelxestrada@gmail.com>
2023-05-24 13:06:29 -0700

    gpt-worded resume updates

--- src/components/files/resume.txt.js
+++ src/components/files/resume.txt.js
@@ -14,60 +14,46 @@ Operating Systems: Windows, macOS, Linux (deb/rpm)\r
 Professional Experience\r
 -----------------------\r
 Release Engineer, PRO Unlimited at Apple Inc., Sunnyvale, CA December 2019 - present\r
-- Creating dynamic, scalable, modern CI solutions for apple.com.\r
-- Creating CLI tools used in CI pipelines.\r
+- Spearhead the creation of dynamic, scalable, and modern CI solutions for apple.com, empowering seamless software releases.\r
+- Develop cutting-edge CLI tools utilized within CI pipelines, streamlining development processes and enhancing efficiency.\r
 \n
 Full Stack Developer, TEKsystems at Apple Inc., Sunnyvale, CA July 2018 - December 2019\r
-- Authored dynamic testing frameworks for components of apple.com using Nodejs (Mocha, Selenium,\r
-Testcafe).\r
-- Made tests portable using Docker-based containers.\r
-- Wrote Jenkins pipelines using Groovy for integrating test harnesses.\r
-- Created multiple report interfaces for test results programmatically for integrating with\r
-existing systems and teams.\r
-- Created tools for testing.\r
+- Pioneered the development of dynamic testing frameworks for Framework components of Apple.com, leveraging nodejs (Mocha, Selenium, TestCafe) to ensure robust software quality.\r
+- Employed Docker-based containers to ensure portability and consistency of tests, greatly improving testing infrastructure.\r
+- Crafted Jenkins pipelines using Groovy, seamlessly integrating test harnesses for efficient continuous integration.\r
+- Engineered multiple report interfaces for test results, programmatically integrating with existing systems and fostering effective collaboration.\r
+- Created problem-specific CLI tools for augmenting manual testing.\r
 \n
 Consultant, Excelon Development at Pingpad.io, Remote February 2018 - March 2018\r
-- Was the sole QA resource for Kanban-style dashboard app for Slack (startup).\r
-- Automated daily testing tasks with JavaScript and Selenium WebDriver.\r
-- Introduced standardized QA documentation.\r
+- Solely responsible for driving quality assurance efforts for a Kanban-style dashboard app for Slack, showcasing exceptional QA skills and attention to detail\r
+- Automated daily testing tasks using JavaScript and Selenium WebDriver, streamlining workflows and improving overall efficiency.\r
+- Introduced standardized QA documentation, enhancing clarity and maintaining consistency in testing processes.\r
 \n
 Senior Support Engineer, American Residential Warranty, Boca Raton, FL April 2016 - May 2018\r
-- Created functional testing framework using Selenium WebDriver (Python).\r
-- Managed pre-production environments (Debian Linux) using Ansible.\r
-- Integrated functional testing framework into Jenkins and Ansible.\r
-- Performed Ad-hoc tests and translated activities into documented test cases.\r
-- Orchestrated back-of-house operations (AD, Azure, five9 VCC) through Python and\r
-PowerShell scripts.\r
+- Developed a comprehensive functional testing framework utilizing Selenium WebDriver (Python), ensuring top-notch software quality.\r
+- Managed pre-production environments (Debian Linux) proficiently using Ansible, guaranteeing smooth deployment and configuration management.\r
+- Integrated the functional testing framework seamlessly into Jenkins and Ansible, optimizing testing processes and enhancing collaboration.\r
+- Orchestrated back-of-house operations (AD, Azure, five9 VCC) through the strategic implementation of Python and PowerShell scripts, streamlining operations and improving overall efficiency.\r
 \n
 Software QA Engineer, Sato Global Solutions, Fort Lauderdale, FL July 2015 - September 2015\r
-- Created test harness for web service written in C#, SQL, JSON.\r
-- Generated nightly test reports for project stakeholders using Jenkins, .NET and ReportUnit.\r
-- Migrated organization's existing SCM solution from SVN to Microsoft TFS.\r
+- Engineered a robust test harness for a web service using C#, SQL, and JSON, ensuring thorough and effective testing.\r
+- Generated comprehensive nightly test reports using Jenkins, .NET, and ReportUnit, enabling stakeholders to make informed decisions.\r
+- Successfully migrated the organization's existing SCM solution from SVN to Microsoft TFS, showcasing strong technical proficiency and project management skills.\r
 \n
 QMO Test Engineer, American Airlines, Phoenix, AZ August 2014 - January 2015\r
-- Created automated test cases with C# using .NET 4.5 for service-layer tests creating\r
-and transmitting XML requests and parsing XML responses, asserting against test\r
-data.\r
-- Created automated test cases using Selenium WebDriver for AUT's UI, using C# and\r
-JavaScript.\r
-- Implemented code to generate pass / fail reports programmatically.\r
-- Integrated C# framework with test cases written in Microsoft Test Manager as a\r
-data source for tests.\r
-- Authored MS SQL queries and stored procedures for validating data in tests.\r
-- Analyzed business logic related to flight and crew scheduling to determine test case\r
-coverage.\r
-- Led and attended code reviews, refactoring legacy code when needed.\r
+- Developed automated test cases using C# and .NET 4.5 for service-layer tests, demonstrating expertise in XML handling and data validation.\r
+- Employed Selenium WebDriver and C# to create automated test cases for the AUT's UI, showcasing proficiency in frontend testing.\r
+- Implemented code to generate detailed pass/fail reports programmatically, enabling efficient result analysis.\r
+- Integrated C# framework with test cases written in Microsoft Test Manager as a data source, optimizing test management and execution.\r
+- Leveraged MS SQL queries and stored procedures for effective data validation in tests, ensuring accuracy and reliability.\r
+- Analyzed complex business logic related to flight and crew scheduling, ensuring comprehensive test coverage.\r
+- Led and participated in code reviews, actively contributing to the improvement of legacy code.\r
 \n
 Senior QA Engineer, L1, Publicis.Sapient, Miami, FL April 2007 - May 2012\r
-- Tested applications built on multiple runtimes and platforms (.NET, Java, Adobe Flash).\r
-- Authored QA documentation for multiple parts of the SDLC (test plans, test suites, test cases,\r
-bugs).\r
-- Performed and documented ad-hoc tests for web applications and services.\r
-- Wrote Powershell and shell scripts automating configuration of Windows and Linux servers for\r
-web applications (IIS, Apache).\r
-- Led initial site migrations for onboarding new clients, integrating them into our pre-production\r
-environments (DEV/QA/UAT).\r
-- Performed Build and Release for various .NET applications (MSBuild, Batch, nAnt, MSSQL).\r
-- Utilized multiple scripting languages for everyday automation tasks and tooling (Python, Bash,\r
-PowerShell).\r
+- Conducted thorough testing on applications built on multiple runtimes and platforms (.NET, Java, Adobe Flash), showcasing versatility and adaptability.\r
+- Authored comprehensive QA documentation, including test plans, test suites, test cases, and bug reports, ensuring effective communication and knowledge transfer.\r
+- Performed meticulous ad-hoc tests for web applications and services, demonstrating a keen eye for detail and a proactive approach to quality assurance.\r
+- Automated server configurations for web applications (IIS, Apache) using PowerShell and shell scripts, enhancing efficiency and accuracy.\r
+- Led site migrations for onboarding new clients, flawlessly integrating them into pre-production environments (DEV/QA/UAT), showcasing strong project management skills.\r
+- Successfully handled build and release processes for various .NET applications, leveraging multiple scripting languages and build tools, including Python, shell, PowerShell, MSBuild, Batch, and nAnt.\r
 `;
