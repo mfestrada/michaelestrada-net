93ec7822680aa5fad562ce57f31ce88fa4f7da66
mfestrada <michaelxestrada@gmail.com>
2023-06-23 11:54:51 -0700

    latest resume tweaks, updated platform skills

--- src/components/files/resume.txt.js
+++ src/components/files/resume.txt.js
@@ -6,10 +6,10 @@ michaelxestrada@gmail.com\r
 Skills\r
 ------\r
 Languages: Java, Groovy, C#, Python, JavaScript (browser & nodejs), TypeScript, SQL, Shell (bash/ksh/sh/zsh)\r
-Databases: MSSQL, MySQL, Django ORM, Sqlite\r
-Tools: Ansible, Jenkins, Gradle, Make\r
-Platform: Artifactory, Vault, Docker, Kubernetes\r
+Databases: MySQL, MSSQL, Postgres, Sqlite\r
+Tools: Ansible, Jenkins, Gradle, Make, Artifactory, Vault, Docker, Kubernetes, Helm\r
 Operating Systems: Windows, macOS, Linux (deb/rpm)\r
+Platform: UNIX, TCP/IP, UDP, SIP, DNS, DCHP, SSL, TLS, HTTP/HTTPS, GraphQL\r
 \n
 Professional Experience\r
 -----------------------\r
@@ -18,14 +18,14 @@ Release Engineer, PRO Unlimited at Apple Inc., Sunnyvale, CA December 2019 - pre
 - Develop cutting-edge CLI tools utilized within CI pipelines, streamlining development processes and enhancing efficiency.\r
 \n
 Full Stack Developer, TEKsystems at Apple Inc., Sunnyvale, CA July 2018 - December 2019\r
-- Pioneered the development of dynamic testing frameworks for Framework components of Apple.com, leveraging nodejs (Mocha, Selenium, TestCafe) to ensure robust software quality.\r
+- Lead the development of dynamic testing frameworks for Framework components of Apple.com, leveraging nodejs (Mocha, Selenium, TestCafe) to ensure quality standards were met.\r
+- Developed authentication library that bridged tests with Apple's internal environments.\r
 - Employed Docker-based containers to ensure portability and consistency of tests, greatly improving testing infrastructure.\r
 - Crafted Jenkins pipelines using Groovy, seamlessly integrating test harnesses for efficient continuous integration.\r
 - Engineered multiple report interfaces for test results, programmatically integrating with existing systems and fostering effective collaboration.\r
 - Created reliable CLI tools for augmenting manual testing and boosting turn-around times.\r
-- Developed authentication library that bridged tests with Apple's internal environments.\r
 \n
-Consultant, Excelon Development at Pingpad.io, Remote February 2018 - March 2018\r
+Consultant, Excelon Development at Pingpad.io, Remote February 2018 - July 2018\r
 - Solely responsible for driving quality assurance efforts for a Kanban-style dashboard app for Slack, showcasing exceptional QA skills and attention to detail.\r
 - Automated daily testing tasks using JavaScript and Selenium WebDriver, streamlining workflows and improving overall efficiency.\r
 - Introduced standardized QA documentation, enhancing clarity and maintaining consistency in testing processes.\r
